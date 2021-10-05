4f10dafa43d09aac5330444921f144b7baf4bcb6
gellison0686 <gellison0686@hwdsb.on.ca>
2021-10-05 12:03:30 -0700

    added wasd movemoent

--- main.py
+++ main.py
@@ -5,21 +5,31 @@ WIDTH, HEIGHT = 1100, 700
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("PAPA G SMELLS")
 
+VEL = 5
+
 RED = (255, 0, 0)
 
 FPS = 60
 
+SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
+
 YELLOW_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Assets', 'spaceship_yellow.png'))
+YELLOW_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(YELLOW_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 90)
 
 RED_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Assets', 'spaceship_red.png'))
+RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 270)
 
-def draw_window():
+def draw_window(red, yellow):
     WIN.fill(RED)
-    WIN.blit(YELLOW_SPACESHIP_IMAGE, (150, 200))
+    WIN.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y))
+    WIN.blit(RED_SPACESHIP, (red.x, red.y))
     pygame.display.update()
 
 
 def main():
+    red = pygame.Rect(900, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+    yellow = pygame.Rect(200, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+
     clock = pygame.time.Clock()
     run = True
     while run:
@@ -28,7 +38,18 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
 
-        draw_window()
+        keys_pressed = pygame.key.get_pressed()
+        if keys_pressed[pygame.K_a]:
+            yellow.x -= VEL
+        if keys_pressed[pygame.K_d]:
+            yellow.x += VEL
+        if keys_pressed[pygame.K_s]:
+            yellow.y += VEL
+        if keys_pressed[pygame.K_w]:
+            yellow.y -= VEL
+
+
+        draw_window(red, yellow)
 
     pygame.quit()
 
