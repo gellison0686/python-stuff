ac367ac411abbf1d850f66f63fee1a2c53114afb
gellison0686 <gellison0686@hwdsb.on.ca>
2021-10-06 12:01:38 -0700

    :)

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ WIDTH, HEIGHT = 1100, 700
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("PAPA G SMELLS")
 
+BORDER = pygame.Rect(WIDTH/2 - 5, 0, 10, HEIGHT)
+
 VEL = 5
 
+BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 
 FPS = 60
@@ -21,10 +24,30 @@ RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(RED_SPACESHIP_IMA
 
 def draw_window(red, yellow):
     WIN.fill(RED)
+    pygame.draw.rect(WIN, BLACK, BORDER)
     WIN.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y))
     WIN.blit(RED_SPACESHIP, (red.x, red.y))
     pygame.display.update()
 
+def yellow_handle_movement(keys_pressed, yellow):
+    if keys_pressed[pygame.K_a] and yellow.x - VEL > 0:
+        yellow.x -= VEL
+    if keys_pressed[pygame.K_d] and yellow.x + VEL + yellow.width < BORDER.x:
+        yellow.x += VEL
+    if keys_pressed[pygame.K_s] and yellow.y + VEL + yellow.height < HEIGHT - 15:
+        yellow.y += VEL
+    if keys_pressed[pygame.K_w] and yellow.y - VEL > 0:
+        yellow.y -= VEL
+
+def red_handle_movement(keys_pressed, red):
+    if keys_pressed[pygame.K_LEFT] and red.x - VEL > BORDER.x + BORDER.width:
+        red.x -= VEL
+    if keys_pressed[pygame.K_RIGHT] and red.x + VEL + red.width < WIDTH:
+        red.x += VEL
+    if keys_pressed[pygame.K_DOWN] and red.y + VEL + red.height < HEIGHT - 15:
+        red.y += VEL
+    if keys_pressed[pygame.K_UP] and red.y - VEL > 0:
+        red.y -= VEL
 
 def main():
     red = pygame.Rect(900, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
@@ -39,16 +62,8 @@ def main():
                 run = False
 
         keys_pressed = pygame.key.get_pressed()
-        if keys_pressed[pygame.K_a]:
-            yellow.x -= VEL
-        if keys_pressed[pygame.K_d]:
-            yellow.x += VEL
-        if keys_pressed[pygame.K_s]:
-            yellow.y += VEL
-        if keys_pressed[pygame.K_w]:
-            yellow.y -= VEL
-
-
+        yellow_handle_movement(keys_pressed, yellow)
+        red_handle_movement(keys_pressed, red)
         draw_window(red, yellow)
 
     pygame.quit()
